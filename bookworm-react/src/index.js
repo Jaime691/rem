3b27ea535e7744e22ed9e22fd9e57dceb15ef3b6
jmayorga <jmayorgadev@gmail.com>
2018-08-03 14:40:31 -0500

    Redux and Redux thunk configured

--- bookworm-react/src/index.js
+++ bookworm-react/src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
 import App from './App';
+import rootReducer from './rootReducer';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root')
 );
