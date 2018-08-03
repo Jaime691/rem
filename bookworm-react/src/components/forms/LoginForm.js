22776a157f1c208044367ba896f745d7a9133f0e
jmayorga <jmayorgadev@gmail.com>
2018-08-03 14:04:54 -0500

    Login Form with validation

--- bookworm-react/src/components/forms/LoginForm.js
+++ bookworm-react/src/components/forms/LoginForm.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Validator from 'validator';
+import InlineError from '../messages/InlineError';
 
 class LoginForm extends Component {
   state = {
     data: { email: '', password: '' },
     loading: false,
-    errors: []
+    errors: {}
   };
 
   onChange = e => {
@@ -13,12 +16,26 @@ class LoginForm extends Component {
     });
   };
 
-  onSubmit = () => {
+  onSubmit = e => {
+    e.preventDefault();
     const errors = this.validate(this.state.data);
     this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
+      this.props.submit(this.state.data);
+    }
   };
+
+  validate = data => {
+    const errors = {};
+    if (!Validator.isEmail(data.email)) errors.email = 'Correo no válido';
+    if (Validator.isEmpty(data.email)) errors.email = 'Vacio';
+    if (Validator.isEmpty(data.password))
+      errors.password = 'No puede estar en blanco.';
+    return errors;
+  };
+
   render() {
-    const { data } = this.state;
+    const { data, errors } = this.state;
     return (
       <form onSubmit={this.onSubmit}>
         <div className="field">
@@ -36,6 +53,7 @@ class LoginForm extends Component {
               onChange={this.onChange}
             />
           </div>
+          {errors.email && <InlineError text={errors.email} />}
         </div>
         <div className="field">
           <label className="label" htmlFor="password">
@@ -52,11 +70,17 @@ class LoginForm extends Component {
               onChange={this.onChange}
             />
           </div>
+          {errors.password && <InlineError text={errors.password} />}
         </div>
-        <button className="button is-link">Login</button>
+        <button className="button" type="submit">
+          Login
+        </button>
       </form>
     );
   }
 }
 
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
+};
 export default LoginForm;
--- bookworm-react/src/components/pages/LoginPage.js
+++ bookworm-react/src/components/pages/LoginPage.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LoginForm from '../forms/LoginForm';
-const LoginPage = () => {
-  return (
-    <div>
-      <h1>LoginPage</h1>
-      <LoginForm />
-    </div>
-  );
-};
+
+class LoginPage extends Component {
+  state = {};
+
+  submit = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>LoginPage</h1>
+        <LoginForm submit={this.submit} />
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
