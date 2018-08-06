1c78fd300b96ca386d508840404b379869c82779
jmayorga <jmayorgadev@gmail.com>
2018-08-06 11:11:37 -0500

    Loading component added

--- bookworm-react/src/components/forms/LoginForm.js
+++ bookworm-react/src/components/forms/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Validator from 'validator';
+import ReactLoading from 'react-loading';
 import InlineError from '../messages/InlineError';
 
 class LoginForm extends Component {
@@ -19,11 +20,15 @@ class LoginForm extends Component {
   onSubmit = e => {
     e.preventDefault();
     const errors = this.validate(this.state.data);
+
     this.setState({ errors });
     if (Object.keys(errors).length === 0) {
+      this.setState({ loading: true });
       this.props
         .submit(this.state.data)
-        .catch(err => this.setState({ errors: err.response.data.errors }));
+        .catch(err =>
+          this.setState({ errors: err.response.data.errors, loading: false })
+        );
     }
   };
 
@@ -37,50 +42,55 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { data, errors } = this.state;
-    console.log(errors);
+    const { data, errors, loading } = this.state;
 
     return (
-      <form onSubmit={this.onSubmit}>
-        {errors.global && <InlineError text="Algo fue mal" />}
-        <div className="field">
-          <label className="label" htmlFor="email">
-            Email
-          </label>
-          <div className="control">
-            <input
-              className="input"
-              type="email"
-              id="email"
-              name="email"
-              placeholder="example@hotmail.com"
-              value={data.email}
-              onChange={this.onChange}
-            />
-          </div>
-          {errors.email && <InlineError text={errors.email} />}
-        </div>
-        <div className="field">
-          <label className="label" htmlFor="password">
-            Password
-          </label>
-          <div className="control">
-            <input
-              className="input"
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Type your password"
-              value={data.password}
-              onChange={this.onChange}
-            />
-          </div>
-          {errors.password && <InlineError text={errors.password} />}
-        </div>
-        <button className="button" type="submit">
-          Login
-        </button>
-      </form>
+      <div>
+        {loading === true ? (
+          <ReactLoading type="spinningBubbles" color="#4a8ee8" />
+        ) : (
+          <form onSubmit={this.onSubmit}>
+            {errors.global && <InlineError text="Algo fue mal" />}
+            <div className="field">
+              <label className="label" htmlFor="email">
+                Email
+              </label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="example@hotmail.com"
+                  value={data.email}
+                  onChange={this.onChange}
+                />
+              </div>
+              {errors.email && <InlineError text={errors.email} />}
+            </div>
+            <div className="field">
+              <label className="label" htmlFor="password">
+                Password
+              </label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Type your password"
+                  value={data.password}
+                  onChange={this.onChange}
+                />
+              </div>
+              {errors.password && <InlineError text={errors.password} />}
+            </div>
+            <button className="button" type="submit">
+              Login
+            </button>
+          </form>
+        )}
+      </div>
     );
   }
 }
